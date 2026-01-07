dcd54cf52952584bfa0b1bef85bce85207a37a62
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-07 07:48:37 +0100

    feat: Add an asynchronous Gitea API client class

--- src/gitea/client/__init__.py
+++ src/gitea/client/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from gitea.client.async_gitea import AsyncGitea
 from gitea.client.gitea import Gitea
 
-__all__ = ["Gitea"]
+__all__ = ["AsyncGitea", "Gitea"]
