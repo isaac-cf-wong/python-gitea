25411dae6129a3027482664e3848637e6cd19e20
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-02 17:22:39 +0100

    chore: Update the `execute_api_command` function

--- src/gitea/cli/utils/__init__.py
+++ src/gitea/cli/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from gitea.cli.utils.api import execute_api_command
 from gitea.cli.utils.auth import get_auth_params
 
-__all__ = ["get_auth_params"]
+__all__ = ["execute_api_command", "get_auth_params"]
