09c7b2a7d33c3661e748586a8ff059dcb13efc26
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-03 06:13:22 +0100

    feat: Add `gitea-cli pull-request` command

--- src/gitea/cli/main.py
+++ src/gitea/cli/main.py
@@ -102,10 +102,12 @@ def register_commands() -> None:
     """Register CLI commands."""
     from gitea.cli.config.main import config_app  # noqa: PLC0415
     from gitea.cli.issue.main import issue_app  # noqa: PLC0415
+    from gitea.cli.pull_request.main import pull_request_app  # noqa: PLC0415
     from gitea.cli.user.main import user_app  # noqa: PLC0415
 
     app.add_typer(config_app, name="config", help="Commands for managing configurations.")
     app.add_typer(issue_app, name="issue", help="Commands for managing issues.")
+    app.add_typer(pull_request_app, name="pull-request", help="Commands for managing pull requests.")
     app.add_typer(user_app, name="user", help="Commands for managing users.")
 
 
