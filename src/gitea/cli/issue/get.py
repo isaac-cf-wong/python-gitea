ad241fbbfdabe0fe748c316a1e57992c9f91f009
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-03 05:36:47 +0100

    docs: Fix the help of `get_command`

--- src/gitea/cli/issue/get.py
+++ src/gitea/cli/issue/get.py
@@ -30,7 +30,7 @@ def get_command(  # noqa: PLR0913
         str | None,
         typer.Option(
             "--base-url",
-            help="Base URL of the GitHub platform. If not provided, the base URL from the specified account will be used.",
+            help="Base URL of the Gitea platform. If not provided, the base URL from the specified account will be used.",
         ),
     ] = None,
 ) -> None:
